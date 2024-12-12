7421aeaa6a339339da5ff4c30df2eb8b4b70a706
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    sm8450-common: Build DSPVolumeSynchronizer
    Change-Id: I46f37b21fc9f0dd7cfb7f1d72ebd07ae5375bb2f

--- common.mk
+++ common.mk
@@ -302,6 +302,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer \
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
