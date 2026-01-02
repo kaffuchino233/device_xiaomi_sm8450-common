01e4030a4d8e45083d7080b5d036dfec6649867c
kaffuchino233 <qkivan@proton.me>
2026-01-03 03:00:55 +0800

    sm8450-common: Add libqti-perfd-client to namespace
    * Fix ELF check

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/xiaomi/sm8450-common',
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
